71e8c086122fcbd19aa4264f533e5a5a1a324e15
unknown <paulo.rosantos@rede.ulbra.br>
2024-06-18 00:44:35 -0300

    first ruby program

---
+++ firstProgram.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Seu Sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite também sua idade: "
+idade = gets.chomp.to_i
+
+puts "Olá #{nome} #{sobrenome} de idade #{idade}"
